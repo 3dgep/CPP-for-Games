a8c5707b89b82598373ea77cb68dbea0008dec41
Jeremiah van Oosten <jeremiah@3dgep.com>
2025-04-02 10:37:37 +0200

    Add union example to Classes/main.cpp.

--- Classes/main.cpp
+++ Classes/main.cpp
@@ -2,6 +2,18 @@
 
 #include <iostream>
 
+union Color
+{
+    uint32_t rgba;
+    struct
+    {
+        uint8_t r;
+        uint8_t g;
+        uint8_t b;
+        uint8_t a;
+    };
+};
+
 void printHitPoints(const Entity& e)
 {
     std::cout << "hitPoints: " << e.getHitPoints() << std::endl;
